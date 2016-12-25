ae29afb681485b42442828b44441676920aa47ee
troyi <ratulbasak93@gmail.com>
2016-12-26 03:09:45 +0600

    Update stt_tts.py
    need to run on every query...

--- stt_tts.py
+++ stt_tts.py
@@ -14,7 +14,26 @@ try:
     user_message = r.recognize_google(audio)
     user_message_str = user_message.lower()
     print("You said " + user_message_str)    # recognize speech using Google Speech Recognition
-    os.system("espeak '"+user_message_str+"'")
+    #os.system("espeak '"+user_message_str+"'")
+    if user_message_str == "what is your name":
+        os.system("espeak 'my_name_is_jarrviss.\n'")
+    elif user_message_str == "jarvis":
+        os.system("espeak 'Yes?\n'")
+    elif user_message_str == "how are you":
+        os.system("espeak 'I_am_Fine,Thank_you.What_about_you?\n'")
+    elif user_message_str == "tell me about you":
+        os.system("espeak 'I_am_jarvis_and_I_am_coded_in_python.My_CPU_is_2.4_gigahertz_and_I_have_intel_core_i3_processor.I_have_4gb_ram.\n'")
+    elif user_message_str == "are you single":
+        os.system("espeak 'yes...but_i_lovee_IRONMAN.\n'")
+    elif user_message_str == "add 20 plus 40":
+        os.system("espeak 'sixty\n'")
+    elif user_message_str == "5 into 5":
+        os.system("espeak 'twentyfive\n'")
+    elif user_message_str == "which search engine do you prefer":
+        os.system("espeak 'I_prefer_google.\n'")
+    elif user_message_str == "shut up":
+        os.system("espeak 'Okay\n'")
+    
 except LookupError:                            # speech is unintelligible
     print("Could not understand audio")
 except sr.UnknownValueError:
